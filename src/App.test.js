66b73ed3ccb0dc8cc46e75774f055920dc822274
Alan Puglisi <alan.c.puglisi@gmail.com>
2020-12-20 14:07:31 -0600

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { BrowserRouter } from "react-router-dom";
+import { ExpansionPanelActions } from '@material-ui/core';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders without crashing', () => {
+  render(<Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>);
+});
+test('matches snapshot', () => {
+  const { asFragment } = render(<Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>);
+
+  expect(asFragment()).toMatchSnapshot();
 });
